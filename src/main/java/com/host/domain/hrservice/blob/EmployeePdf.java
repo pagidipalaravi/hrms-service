2f3912c21f2b9ba8545ff95734590330e9af6d9b
pagidipalaravi <pagidipala.ravi@gmail.com>
2024-09-05 18:26:57 +0530

    dependencies
    dependencies

--- src/main/java/com/host/domain/hrservice/blob/EmployeePdf.java
+++ src/main/java/com/host/domain/hrservice/blob/EmployeePdf.java
@@ -1,28 +1,29 @@
 package com.host.domain.hrservice.blob;
-import java.io.*;  
-import java.sql.*;  
-public class EmployeePdf {
-	
-	public static void main(String[] args) {  
-	try{  
-	Class.forName("oracle.jdbc.driver.OracleDriver");  
-	Connection con=DriverManager.getConnection(  
-	"jdbc:oracle:thin:@localhost:1521:xe","hr","hr");  
-	              
-	PreparedStatement ps=con.prepareStatement(  
-	"insert into employees values(?,?)");  
-	              
-	File f=new File("E:\\u01\\employeeresume.pdf");  
-	FileReader fr=new FileReader(f);  
-	              
-	ps.setInt(1,101);  
-	ps.setCharacterStream(2,fr,(int)f.length());  
-	int i=ps.executeUpdate();  
-	System.out.println(i+" records affected");  
-	              
-	con.close();  
-	              
-	}catch (Exception e) {e.printStackTrace();}  
-	}  
-	}  
 
+import java.io.*;
+import java.sql.*;
+
+public class EmployeePdf {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+
+			PreparedStatement ps = con.prepareStatement("insert into employees values(?,?)");
+
+			File f = new File("E:\\u01\\employeeresume.pdf");
+			FileReader fr = new FileReader(f);
+
+			ps.setInt(1, 101);
+			ps.setCharacterStream(2, fr, (int) f.length());
+			int i = ps.executeUpdate();
+			System.out.println(i + " records affected");
+
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
